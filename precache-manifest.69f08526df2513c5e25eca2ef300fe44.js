594642d0687967a6f88ecbdfc0cc58a17ef6a787
aidaninja <junceliz@gmail.com>
2019-09-12 00:06:41 +0900

    Updates

---
+++ precache-manifest.69f08526df2513c5e25eca2ef300fe44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa206ed11f4f5ec3e0973336bdf11a47",
+    "url": "/StackNRead_front/index.html"
+  },
+  {
+    "revision": "5c8410e04c9ac87ae429",
+    "url": "/StackNRead_front/static/css/main.0b4c86c1.chunk.css"
+  },
+  {
+    "revision": "1acf0fae6e66b05ae931",
+    "url": "/StackNRead_front/static/js/2.512c3c3e.chunk.js"
+  },
+  {
+    "revision": "5c8410e04c9ac87ae429",
+    "url": "/StackNRead_front/static/js/main.b534d290.chunk.js"
+  },
+  {
+    "revision": "47b9d67c5cff5dead554",
+    "url": "/StackNRead_front/static/js/runtime~main.896a62d4.js"
+  },
+  {
+    "revision": "0442d746d48cccc006fd9d295b3370b7",
+    "url": "/StackNRead_front/static/media/top_placeholder.0442d746.jpg"
+  }
+]);
